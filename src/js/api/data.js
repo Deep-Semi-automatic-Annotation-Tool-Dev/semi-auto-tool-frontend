c335559fbfe062b851ffb73f73d2845e19cd3a44
K1A2 <jckim0307@gmail.com>
2023-05-16 13:10:31 +0900

    [FEAT] save

--- src/js/api/data.js
+++ src/js/api/data.js
@@ -132,11 +132,10 @@ export const deleteTagInData = async (context, projectId, targetTag, targetDataI
     console.log(newTags)
 
     try {
-        const result = await axios.put(`${context.$baseURL}api/v1/project/${projectId}/data`, [{
-            "id": targetData.id,
-            "text": targetData.text,
+        const result = await axios.put(`${context.$baseURL}api/v1/project/${projectId}/data/${targetData.id}`,
+            {
             "data_tags": newTags
-        }])
+        })
         console.log(result.data[0].data_tags);
         targetData.data_tags = result.data[0].data_tags
     } catch (error) {
@@ -145,6 +144,40 @@ export const deleteTagInData = async (context, projectId, targetTag, targetDataI
         context.showLoadingDialog = false
     }
 }
+
+// export const deleteTagInWord = async (context) => {
+//     context.showLoadingDialog = true
+//     context.loadingDialogTitle = '단어 태그 삭제'
+//     context.loadingDialogSubTitle = '단어에 할당된 태그 제거 중...'
+//
+//     const targetData = context.lineData[targetDataIdx]
+//     const newTags = []
+//     for (let tIdx in targetData.data_tags) {
+//         const target = targetData.data_tags[tIdx]
+//         const insertData = {}
+//         if (target.tagId === targetTag.tagId) {
+//             continue
+//         }
+//         insertData.tag_group_id = target.tagGroupId
+//         insertData.tag_id = target.tagId
+//         newTags.push(insertData)
+//     }
+//     console.log(newTags)
+//
+//     try {
+//         const result = await axios.put(`${context.$baseURL}api/v1/project/${projectId}/data`, [{
+//             "id": targetData.id,
+//             "text": targetData.text,
+//             "data_tags": newTags
+//         }])
+//         console.log(result.data[0].data_tags);
+//         targetData.data_tags = result.data[0].data_tags
+//     } catch (error) {
+//         console.error('put tag delete in data error', error);
+//     } finally {
+//         context.showLoadingDialog = false
+//     }
+// }
 
 export const postData = async (context, projectId, file, colName) => {
     context.showLoadingDialog = true
